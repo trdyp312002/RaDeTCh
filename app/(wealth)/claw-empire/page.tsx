0f81ba1711935f6cafb7e3e6ee6a28603b41c427
trdyp312002 <trdyp312002@gmail.com>
2026-06-07 13:39:59 +0900

    fix: Use NEXT_PUBLIC_CLAW_EMPIRE_URL for iframe src

--- app/(wealth)/claw-empire/page.tsx
+++ app/(wealth)/claw-empire/page.tsx
@@ -1,10 +1,12 @@
 "use client"
 
 export default function ClawEmpirePage() {
+  const clawEmpireUrl = process.env.NEXT_PUBLIC_CLAW_EMPIRE_URL || "http://localhost:8800";
+  
   return (
     <div className="w-full" style={{ height: "calc(100vh - 4rem)" }}>
       <iframe
-        src="http://localhost:8800"
+        src={clawEmpireUrl}
         className="w-full h-full border-0"
         allow="clipboard-read; clipboard-write"
         title="Claw Empire"
